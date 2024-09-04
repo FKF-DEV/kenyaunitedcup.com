a20dfb17f5c86f65d5790f8e958692776589c825
bridgitkanini <bridgit.k.mutinda@gmail.com>
2024-09-04 15:14:15 +0300

    Add logic to fetch resources data from API

--- src/pages/home/section/ElectionsFiles.jsx
+++ src/pages/home/section/ElectionsFiles.jsx
@@ -1,47 +1,29 @@
 /* eslint-disable react/prop-types */
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { cloudDownload } from "../../../assets";
 import { Header } from "../../../components";
 
-const electionFiles = [
-  {
-    id: 1,
-    title: "Polling Agents Declaration Form",
-    fileType: "Pdf",
-    size: "2.5mb",
-  },
-  {
-    id: 2,
-    title: "County Elections Preliminary  Results",
-    fileType: "Pdf",
-    size: "3.0mb",
-  },
-  {
-    id: 3,
-    title: "Electoral Rules and Guidelines",
-    fileType: "Pdf",
-    size: "2.5mb",
-  },
-  {
-    id: 4,
-    title: "Final Voter Register",
-    fileType: "Pdf",
-    size: "2.5mb",
-  },
-  {
-    id: 5,
-    title: "FKF Constitution",
-    fileType: "Pdf",
-    size: "2.5mb",
-  },
-  {
-    id: 6,
-    title: "FKF Electoral Code",
-    fileType: "Pdf",
-    size: "2.5mb",
-  },
-];
-
 function ElectionsFiles() {
+  const [resources, setResources] = useState([]);
+  const BASE_URL = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    const fetchResources = async () => {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}/api/resources/resourcetypes/{resourcetypes_id}/resources/`
+        );
+        setResources(response.data);
+      } catch (error) {
+        console.error("Error fetching resources:", error);
+      }
+    };
+
+    fetchResources();
+  }, [BASE_URL]);
+
   return (
     <div className="max-w-7xl h-[664px] mx-auto flex-between flex-col md:flex-row">
       <div className="flex-1 h-full flex-center md:flex-start flex-col gap-2.5 pr-12">
@@ -67,7 +49,7 @@ function ElectionsFiles() {
       </div>
 
       <div className="w-full md:w-1/2 flex-1 flex flex-col gap-4 overflow-y-scroll h-full px-2 md:px-0 py-2 scrollbar">
-        {electionFiles.map((file) => (
+        {resources.map((file) => (
           <FileCard key={file.id} file={file} />
         ))}
       </div>
@@ -78,23 +60,22 @@ function ElectionsFiles() {
 export default ElectionsFiles;
 
 const FileCard = ({ file }) => {
-  const { title, fileType, size } = file;
+  const { title, document } = file;
+
   return (
     <div className="w-full bg-white rounded-xl p-6 flex flex-col">
       <div className="flex-between">
         <h5 className="text-base font-semibold">{title}</h5>
         <button type="button" className="size-8">
-          <img
-            src={cloudDownload}
-            alt="cloud-download"
-            className="size-full object-contain"
-          />
+          <a href={document} target="_blank" rel="noopener noreferrer">
+            <img
+              src={cloudDownload}
+              alt="cloud-download"
+              className="size-full object-contain"
+            />
+          </a>
         </button>
       </div>
-
-      <p className="uppercase text-sm font-normal text-[#475467]">
-        {fileType} • {size}
-      </p>
     </div>
   );
 };
