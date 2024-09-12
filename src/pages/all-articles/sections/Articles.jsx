73eb2e1823d6e64dbc14238f2eb5ea67ea0011e9
bridgitkanini <bridgit.k.mutinda@gmail.com>
2024-09-12 21:49:31 +0300

    Create grid layout for articles display.

--- src/pages/all-articles/sections/Articles.jsx
+++ src/pages/all-articles/sections/Articles.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { calendar, clock } from "../../../assets";
 import { FaClock, FaCalendar } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
@@ -27,39 +27,147 @@ const Articles = () => {
   }, [BASE_URL]);
 
   return (
-    <div>
-      {articles.length === 0 ? (
-        <p>No articles available.</p>
-      ) : (
-        <div>
-          {articles.map((article) => (
-            <div key={article.id}>
-              <img src={article.image} alt={article.title} />
-              <p>{article.title}</p>
-              <p>{article.description}</p>
-              <div className="flex items-center gap-4 md:gap-8 mt-2 md:mt-0">
-                <div className="flex items-center gap-1.5">
-                  <FaClock alt="clock" className="opacity-80" />
-                  <span className="text-base">
-                    {new Date(article.created_at).toLocaleTimeString([], {
-                      hour: "2-digit",
-                      minute: "2-digit",
-                    })}
-                  </span>
-                </div>
-                <div className="flex items-center gap-1.5">
-                  <FaCalendar alt="calendar" className="opacity-80"/>
-                  <span className="text-base">
-                    {new Date(article.created_at).toLocaleDateString([], {
-                      year: "numeric",
-                      month: "short",
-                      day: "numeric",
-                    })}
-                  </span>
+    <div className="container m-20 max-w-[1440px]">
+      {articles.length > 0 && (
+        <div className="grid gap-20">
+          {/* First Div: First 3 Articles */}
+          <div className="grid grid-cols-4 gap-24">
+            {/* Article 1: Large on the left */}
+            <Link
+              to={`/news/${articles[0].title_slug}`}
+              onClick={() => window.scrollTo(0, 0)}
+              className="col-span-2 flex flex-col hover:scale-105 transition-all duration-300 ease-in"
+            >
+              <img
+                src={articles[0].image}
+                alt={articles[0].title}
+                className="w-full h-auto object-cover rounded-lg"
+              />
+              <div className="flex flex-col justify-between mt-16 gap-4 w-[75%]">
+                <h2 className="text-2xl font-bold">{articles[0].title}</h2>
+                <p className="text-base line-clamp-2">
+                  {articles[0].description}
+                </p>
+                <div className="flex items-center gap-4 mt-4">
+                  <div className="flex items-center gap-1.5">
+                    <FaClock className="opacity-80" />
+                    <span className="text-base">
+                      {new Date(articles[0].created_at).toLocaleTimeString([], {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                      })}
+                    </span>
+                  </div>
+                  <div className="flex items-center gap-1.5">
+                    <FaCalendar className="opacity-80" />
+                    <span className="text-base">
+                      {new Date(articles[0].created_at).toLocaleDateString([], {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </span>
+                  </div>
                 </div>
               </div>
+            </Link>
+
+            {/* Articles 2 and 3: Side by side on the right */}
+            <div className="flex flex-col gap-10">
+              {articles.slice(1, 3).map((article, index) => (
+                <Link
+                  to={`/news/${article.title_slug}`}
+                  onClick={() => window.scrollTo(0, 0)}
+                  key={article.id}
+                  className="flex flex-row gap-4  hover:scale-105 transition-all duration-300 ease-in"
+                >
+                  <img
+                    src={article.image}
+                    alt={article.title}
+                    className="w-full h-[370px] object-cover rounded-lg"
+                  />
+                  <div className="flex flex-col gap-4 min-w-[300px]">
+                    <h3 className="text-xl font-bold">{article.title}</h3>
+                    <p className="text-base line-clamp-3">
+                      {article.description}
+                    </p>
+                    <div className="flex items-center gap-4 mt-4">
+                      <div className="flex items-center gap-2">
+                        <FaClock className="opacity-80" />
+                        <span className="text-base">
+                          {new Date(articles[0].created_at).toLocaleTimeString(
+                            [],
+                            {
+                              hour: "2-digit",
+                              minute: "2-digit",
+                            }
+                          )}
+                        </span>
+                      </div>
+                      <div className="flex items-center gap-1.5">
+                        <FaCalendar className="opacity-80" />
+                        <span className="text-base">
+                          {new Date(articles[0].created_at).toLocaleDateString(
+                            [],
+                            {
+                              year: "numeric",
+                              month: "short",
+                              day: "numeric",
+                            }
+                          )}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              ))}
             </div>
-          ))}
+          </div>
+
+          {/* Second Div: Articles 4 and 5 */}
+          <div className="flex flex-row">
+            {articles.slice(3, 5).map((article) => (
+              <Link
+                to={`/news/${article.title_slug}`}
+                onClick={() => window.scrollTo(0, 0)}
+                key={article.id}
+                className="flex flex-row gap-4  hover:scale-105 transition-all duration-300 ease-in"
+              >
+                <img
+                  src={article.image}
+                  alt={article.title}
+                  className="w-[43%] h-[370px] object-cover rounded-lg"
+                />
+                <div className="flex flex-col gap-4 max-w-[300px]">
+                  <h3 className="text-xl font-bold">{article.title}</h3>
+                  <p className="text-base line-clamp-3">
+                    {article.description}
+                  </p>
+                  <div className="flex items-center gap-4 mt-4">
+                    <div className="flex items-center gap-1.5">
+                      <FaClock className="opacity-80" />
+                      <span className="text-base">
+                        {new Date(article.created_at).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </span>
+                    </div>
+                    <div className="flex items-center gap-1.5">
+                      <FaCalendar className="opacity-80" />
+                      <span className="text-base">
+                        {new Date(article.created_at).toLocaleDateString([], {
+                          year: "numeric",
+                          month: "short",
+                          day: "numeric",
+                        })}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       )}
     </div>
