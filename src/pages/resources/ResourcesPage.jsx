3f4c5a0bbd08ae9addb4be61fdf8b50a1244c1c3
bridgitkanini <bridgit.k.mutinda@gmail.com>
2024-10-08 19:57:28 +0300

    Add document cards to resources page

--- src/pages/resources/ResourcesPage.jsx
+++ src/pages/resources/ResourcesPage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Hero from './sections/Hero'
 import { Border } from '../../components'
+import DocumentCards from './sections/DocumentCards'
 
 const ResourcesPage = () => {
   return (
-    <div>
+    <main className="bg-gray-100 overflow-hidden">
       <Hero />
       <Border />
-    </div>
+      <DocumentCards />
+    </main>
   )
 }
 
